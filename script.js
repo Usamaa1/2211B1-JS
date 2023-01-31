500b7fd197ae07b89f0ec5496915f311b6d01078
asp <asp@aptechnn2.edu>
2023-01-31 10:42:06 +0500

    JS file 1 updated

---
+++ script.js
@@ -0,0 +1,62 @@
+// var a = 56;
+
+// var a = 67;
+
+
+
+// console.log(a)
+
+// let b = 56;
+
+// b = 67;
+
+// console.log(b)
+
+
+// console.log(4 + 8)
+
+
+
+// let userName = 'Afaz'
+
+
+
+
+
+
+// document.write(userName)
+
+
+
+
+// let userName = prompt('Enter your name')
+// let lastName = prompt('Enter your last name')
+
+// document.write(userName +" "+ lastName)
+
+
+// let num1 = prompt('Enter number 1')
+// let num2 = prompt('Enter number 2')
+
+
+// let convertedNumber = num1;
+
+// let convertedNumber2 = Number(num2)
+
+// document.write(parseInt(num1) + parseInt(num2))
+// document.write(convertedNumber + convertedNumber2)
+
+// console.log(num1 * num2)
+
+// console.log(parseInt(num1) + parseInt(num2))
+
+
+// confirm('jkjfsdkajfksd')
+
+
+
+
+
+
+
+
